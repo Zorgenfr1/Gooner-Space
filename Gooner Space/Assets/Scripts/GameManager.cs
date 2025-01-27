7fd77561e7901a124f67317d34b4bd43000f896d
Zorgenfr1 <frodesfmortensen@gmail.com>
2025-01-27 14:01:29 +0100

    Goon with Chris Trans

--- Gooner Space/Assets/Scripts/GameManager.cs
+++ Gooner Space/Assets/Scripts/GameManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public int playerScore = 0;
     public static GameManager instance;
+
+    public TMP_Text Pointstext;
     void Start()
     {
         if (instance != null)
@@ -18,6 +21,6 @@ public class GameManager : MonoBehaviour
     {
         playerScore += points;
         Debug.Log("Player's current score: " + playerScore);
-
+        Pointstext.text = "POINTS: " + playerScore;
     }
 }
